4c2fb88822126622197dd542ed692dc148d329b5
Keaton Helm <keatonhelm22@gmail.com>
2020-04-09 16:31:56 -0600

    Models added, conigs with iis/startus

---
+++ RockeyWC/Models/FakeProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PupMart.Models {
+
+    public class FakeProductRepository /*: IProductRepository */{
+
+        public IQueryable<Product> Products => new List<Product> {
+            new Product { Name = "Greyhound", Price = 150 },
+            new Product { Name = "Mastiff", Price = 500 },
+            new Product { Name = "Retriever", Price = 95 }
+        }.AsQueryable<Product>();
+    }
+}
---
+++ RockeyWC/Models/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+namespace PupMart.Models {
+
+    // Provides access to order objects
+    public interface IOrderRepository {
+
+        IQueryable<Order> Orders { get; }
+        void SaveOrder(Order order);
+    }
+}
